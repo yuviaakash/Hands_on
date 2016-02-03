5a460827631ade9e8604d96bf29041579d3d7ede
yuviaakash <yuviaakash17@gmail.com>
2016-02-03 21:15:06 +0530

    adding the event handler to the arrow-next class

--- animate.js
+++ animate.js
@@ -7,6 +7,14 @@ var main = function()
 	{
 		$('.dropdown-menu').toggle();
 	});
+//craete the fadeIn and fadeOut effect for the slider
+	$('.arrow-next').click(function()
+	{
+		var currentSlide = $('.active-slide');
+		var nextSlide = currentSlide().next;
+		currentSlide.fadeOut(600).removeClass('.active-slide');
+		nextSlide.fadeIn(600).addClass('.active-slide');
+	});
 }
 
 $(document).ready(main);
